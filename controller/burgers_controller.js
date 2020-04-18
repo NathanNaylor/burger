0b3976f9f7a1a9e42aa77d30b4f0fc2119261557
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-04-17 19:28:49 -0700

    setup basic contrtoller

---
+++ controller/burgers_controller.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+const burger = require("../models/burger");
+
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        const hbsObject = {
+            burgers: data,
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.create({ name: req.body.name, sleepy: req.body.sleepy }, (result) => {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    const condition = { id: req.params.id };
+    const update = { devoured: req.body.value };
+
+    burger.update(update, condition, (result) => {
+        if (result.affectedRows === 0) {
+            // If no rows were affected, then the ID must not exist, so 404
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
