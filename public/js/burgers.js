dc3d74646b20a65ad09e96f3549f7efca389f286
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-04-17 21:54:25 -0700

    fixed file layout

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,18 +1,31 @@
-$(".new-burger").on("click", function() {
-    event.preventDefault();
+    $(".new-burger").on("click", function() {
+        event.preventDefault();
 
-    const newBurger = {
-        burger_name: $("#name").val().trim(),
-        devoured: false
-    };
+        const newBurger = {
+            burger_name: $("#name").val().trim(),
+            devoured: false
+        };
 
-    console.log(newBurger)
+        console.log(newBurger)
 
-    $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-    }).then(() => {
-        console.log("hello")
-        location.reload();
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(() => {
+            location.reload();
+        });
     });
-});
+
+    $(".devour").on("click", function() {
+        const id = $(this).data("id");
+        const updatedBurger = {
+            devoured: 1
+        };
+
+        $.ajax(`/api/burgers/${id}`, {
+            type: "PUT",
+            data: updatedBurger
+        }).then(() => {
+            location.reload();
+        })
+    });
