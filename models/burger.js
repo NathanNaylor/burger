d18f00c407a62504a65d5407b2611b0f9de03dff
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-04-17 19:29:40 -0700

    Created viable burger model

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+const orm = require("../config/orm")
+
+const burger = {
+    all: (cb) => {
+        orm.all("burgers", (res) => {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: (newBurger, cb) => {
+        orm.create("burgers", newBurger, (res) => {
+            cb(res);
+        });
+    },
+    update: (burgerData, criteria, cb) => {
+        orm.update("burgers", burgerData, criteria, (res) => {
+            cb(res);
+        });
+    },
+};
+
+module.exports = burger;
