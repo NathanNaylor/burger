ee5da6492259aac74b01bfb9e0954c07755b3428
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-04-17 19:29:06 -0700

    established working database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("In n Out Burger");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Five Guys Burger");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac");
